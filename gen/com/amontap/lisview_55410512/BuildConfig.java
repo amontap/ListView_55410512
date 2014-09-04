d700588ead0889886b913d69c57fd036f1ffe1a1
amontap <boat_111333@hotmail.com>
2014-09-04 15:36:29 +0700

    first commit

---
+++ gen/com/amontap/lisview_55410512/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.amontap.lisview_55410512;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/amontap/lisview_55410512/List_items.java
@@ -0,0 +1,21 @@
+package com.amontap.lisview_55410512;
+
+import android.graphics.drawable.Drawable;
+
+public class List_items {
+	private Drawable drwable;
+	private String title;
+	public Drawable getDrwable() {
+		return drwable;
+	}
+	public void setDrwable(Drawable drwable) {
+		this.drwable = drwable;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
